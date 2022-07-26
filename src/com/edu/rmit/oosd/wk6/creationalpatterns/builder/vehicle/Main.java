16007cb499e5e6fb622cb85016b9c135bcf350cc
Carl. K <48504139+carlkarama@users.noreply.github.com>
2022-07-26 11:35:45 +1000

    fix: moved phone builder to creation directory

--- src/com/edu/rmit/oosd/wk6/creationalpatterns/builder/vehicle/Main.java
+++ src/com/edu/rmit/oosd/wk6/creationalpatterns/builder/vehicle/Main.java
@@ -1,7 +1,5 @@
 package com.edu.rmit.oosd.wk6.creationalpatterns.builder.vehicle;
 
-import com.edu.rmit.oosd.wk6.creationalpatterns.Motorcycle;
-
 public class Main {
 
     public static void main(String[] args) {
--- src/com/edu/rmit/oosd/wk8/structuralpatterns/builder/phoneservice/PhoneBuilder.java
+++
@@ -1,6 +0,0 @@
-package com.edu.rmit.oosd.wk8.structuralpatterns.builder.phoneservice;
-
-public class PhoneBuilder {
-
-
-}

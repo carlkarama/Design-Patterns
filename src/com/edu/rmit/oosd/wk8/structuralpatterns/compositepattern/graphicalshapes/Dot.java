ecfd6ce9cc77ef1543bc93f5e3266b5391c817ea
carlkarama <carlkarama4@gmail.com>
2022-11-05 23:51:19 +1100

    feat: added composite pattern for image editor

---
+++ src/com/edu/rmit/oosd/wk8/structuralpatterns/compositepattern/graphicalshapes/Dot.java
@@ -0,0 +1,4 @@
+package com.edu.rmit.oosd.wk8.structuralpatterns.compositepattern.graphicalshapes;
+
+public class Dot extends BaseShape {
+}
---
+++ src/com/edu/rmit/oosd/wk8/structuralpatterns/compositepattern/graphicalshapes/Rectangle.java
@@ -0,0 +1,5 @@
+package com.edu.rmit.oosd.wk8.structuralpatterns.compositepattern.graphicalshapes;
+
+public class Rectangle extends BaseShape {
+
+}
